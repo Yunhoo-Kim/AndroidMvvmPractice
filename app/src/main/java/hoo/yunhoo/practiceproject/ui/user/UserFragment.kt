6ef45ef1628e45fbe626d7b39dd012d9087f4d3d
Yunhoo Kim <zkfltm3896@gmail.com>
2018-08-31 16:16:45 +0900

    add google ad mob

--- app/src/main/java/hoo/yunhoo/practiceproject/ui/user/UserFragment.kt
+++ app/src/main/java/hoo/yunhoo/practiceproject/ui/user/UserFragment.kt
@@ -1,7 +1,6 @@
 package hoo.yunhoo.practiceproject.ui.user
 
 import android.arch.lifecycle.ViewModelProviders
-import android.content.Context
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -9,14 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
-import dagger.android.support.AndroidSupportInjection
 import hoo.yunhoo.practiceproject.R
 import hoo.yunhoo.practiceproject.base.BaseFragment
 import hoo.yunhoo.practiceproject.databinding.FragmentUserListBinding
 import hoo.yunhoo.practiceproject.injection.ViewModelFactory
-import kotlinx.android.synthetic.main.fragment_user_list.*
 import javax.inject.Inject
 
 
@@ -37,19 +33,9 @@ class UserFragment: BaseFragment() {
         val adRequest = AdRequest.Builder().build()
         super.onAttach(context)
         val ad: AdView = binding.adView
-//        ad.adSize = AdSize.BANNER
-//        ad.adUnitId = "ca-app-pub-3940256099942544/6300978111"
         ad.loadAd(adRequest)
 
         return binding.root
     }
 
-//    override fun onAttach(context: Context?) {
-//        val adRequest = AdRequest.Builder().build()
-//        super.onAttach(context)
-//        val ad: AdView = binding.adView
-//        ad.adSize = AdSize.BANNER
-//        ad.adUnitId = "ca-app-pub-3940256099942544/6300978111"
-//        ad.loadAd(adRequest)
-//    }
 }
